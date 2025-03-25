6d2558590e814305c07623872aaef2c912a33feb
Georgem Arruda <georgem.junior@meutudo.app>
2025-03-25 14:48:09 -0300

    Ajustes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('EngageSphere', () => {
   beforeEach(() => {
     cy.visit('/')
   })
@@ -9,14 +9,14 @@ describe('template spec', () => {
   })
 
   it('Exibe a saudação "Hi, Joe" quando o nome é fornecido', () => {
-    cy.get('input[data-testid="name"]').clear().type('Joe')
+    cy.get('input[data-testid="name"]').type('Joe')
     cy.contains('h2','Hi Joe!').should('be.visible')
   })
 
   it('Abre e fecha o messenger ', () => {
-    cy.get('button[class*=Messenger]').first().should('be.visible').click()
+    cy.get('button[aria-label="Open messenger"]').click()
     cy.contains('How can we help you?').should('be.visible')
-    cy.get('button[class*=Messenger]').last().should('be.visible').click()
+    cy.get('button[aria-label="Close messenger"]').click()
     cy.get('div[class*=Messenger_box]').should('not.exist')
   })
 })
